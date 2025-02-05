5b75ae0e78c43e573c3b6d78d51a5023a3efa277
D1ZEASE420 <heinlaidjuhan@gmail.com>
2025-02-05 10:23:03 +0200

    update

--- app/Console/Commands/TimetableNotification.php
+++ app/Console/Commands/TimetableNotification.php
@@ -55,7 +55,7 @@ class TimetableNotification extends Command
         ->values();
 
     //return $data;
-    Mail::to('heinlaidjuhan@gmail.ee')->send(new Timetable($data));
+    Mail::to('heinlaidjuhan@gmail.com')->send(new Timetable($data));
 
     }
 
